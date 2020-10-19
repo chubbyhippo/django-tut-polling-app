8198f27dd4db74c28a24cc4e092a4a997dbc4480
ch <mkmsacc@outlook.com>
2020-10-19 19:58:53 +0700

    add results

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
-    # ex: /pols/5/
+    # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
 ]
